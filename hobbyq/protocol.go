7408b5dcb51d4329306429a0b1b9f90dbf71902c
Greg Ward <greg@gerg.ca>
2021-03-18 21:10:36 -0400

    Start implement wire protocol in the server
    Enough to read commands and arguments. Also implement one empty
    command: XMAKE.

--- hobbyq/protocol.go
+++ hobbyq/protocol.go
@@ -4,6 +4,9 @@ package hobbyq
 // (Hmmmm: these need to be available in a client library, so
 // probably a separate package!)
 
-const RESP_HANDSHAKE_OK = "200\x00"
-const RESP_BAD_HANDSHAKE = "450\x00"
-const RESP_UNSUPPORTED_VERSION = "451\x00"
+var (
+	RESP_HANDSHAKE_OK = []byte("200\x00")
+	RESP_BAD_SYNTAX = []byte("400\x00")
+	RESP_UNKNOWN_CMD = []byte("405\x00")
+	RESP_UNSUPPORTED_VERSION = []byte("451\x00")
+)
--- hobbyq/server.go
+++ hobbyq/server.go
@@ -5,6 +5,7 @@ package hobbyq
 import (
 	"bytes"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -34,30 +35,41 @@ func (server *Server) Run() error {
 			return nil
 		}
 		log.Printf("received client connection: %s", conn.RemoteAddr())
-		go server.handleConnection(conn)
+		go handleConnection(conn)
 	}
 
 	return nil
 }
 
 // Handle a single client connection, for however long that might take.
-func (server *Server) handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	err := server.handshake(conn)
+	err := handshake(conn)
 	if err != nil {
 		log.Printf("error: %s", err)
 		return
 	}
+	for {
+		err = processCommand(conn)
+		if err == io.EOF {
+			log.Printf("client disconnected")
+			break
+		} else if err != nil {
+			log.Printf("error: %s", err)
+			conn.Write(RESP_BAD_SYNTAX)
+			break
+		}
+	}
 }
 
-func (server *Server) handshake(conn io.ReadWriter) error {
+func handshake(conn io.ReadWriter) error {
 	// The first handshake line from the client is fixed length:
 	//   HQ 0000\n
 	// where 0000 is the client's protocol version (hex-encoded,
 	// unsigned 16-bit integer)
-	resp := RESP_BAD_HANDSHAKE			// assume the worst
-	respond := func() { conn.Write([]byte(resp)) }
+	resp := RESP_BAD_SYNTAX			// assume the worst
+	respond := func() { conn.Write(resp) }
 	defer respond()
 
 	buf := make([]byte, 8)
@@ -84,4 +96,91 @@ func (server *Server) handshake(conn io.ReadWriter) error {
 	// Successful handshake.
 	resp = RESP_HANDSHAKE_OK
 	return nil
+}
+
+// Read and process a single command. If client input is malformed and
+// cannot be parsed, return an error. Otherwise respond to the client,
+// possibly with an error response, and return nil.
+func processCommand(conn io.ReadWriter) error {
+	cmd, err := readString(conn)
+	log.Printf("cmd = %q, err = %v", cmd, err)
+	if err != nil {
+		return err
+	}
+	args, err := readStringArray(conn)
+	log.Printf("args = %q, err = %v", args, err)
+	if err != nil {
+		return err
+	}
+	cfunc := COMMAND_FUNC[cmd]
+	if cfunc == nil {
+		conn.Write(RESP_UNKNOWN_CMD)
+		return nil
+	}
+	return cfunc(args)
+}
+
+func readString(conn io.Reader) (value string, err error) {
+	length, err := readLengthByte(conn)
+	if err != nil {
+		return
+	}
+	data := make([]byte, length)
+	n, err := conn.Read(data)
+	log.Printf("string: n=%v, err=%v, data=%q", n, err, data)
+	if err != nil {
+		return
+	}
+	if n < length {
+		return value, io.ErrUnexpectedEOF
+	}
+	value = string(data)
+
+	return
+}
+
+func readStringArray(conn io.Reader) (values []string, err error) {
+	length, err := readLengthByte(conn)
+	if err != nil {
+		return
+	}
+
+	values = make([]string, length)
+	for i := 0; i < length; i++ {
+		var val string
+		val, err = readString(conn)
+		if err != nil {
+			return
+		}
+		values[i] = val
+	}
+	return
+}
+
+func readLengthByte(conn io.Reader) (value int, err error) {
+	var lengthBuf [1]byte
+	n, err := conn.Read(lengthBuf[0:1])
+	log.Printf("length byte: n=%v, err=%v, buf=%q", n, err, lengthBuf)
+	if err != nil {
+		return
+	}
+	if n < 1 {
+		err = io.ErrUnexpectedEOF
+		return
+	}
+	value = int(lengthBuf[0])
+	return
+}
+
+func cmd_XMAKE(args []string) (err error) {
+	if len(args) != 1 {
+		err = errors.New("XMAKE: require exactly one argument")
+		return
+	}
+
+	return
+}
+
+var COMMAND_FUNC = map[string] func([]string) error{
+	"XMAKE": cmd_XMAKE,
 }
