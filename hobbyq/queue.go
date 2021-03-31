f0a5dd111b1cff5400b091802330ba73ecc53413
Greg Ward <greg@gerg.ca>
2021-03-30 21:10:36 -0400

    Ensure queues get included in DUMP json output too

--- hobbyq/queue.go
+++ hobbyq/queue.go
@@ -1,9 +1,23 @@
 package hobbyq
 
+import (
+	"encoding/json"
+)
+
 type Queue struct {
 	name string
 }
 
 func NewQueue(name string) *Queue {
 	return &Queue{name}
+}
+
+func (queue *Queue) String() string {
+	return "<Queue: " + queue.name + ">"
+}
+
+func (queue *Queue) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string] interface{}{
+		"name": queue.name,
+	})
 }
